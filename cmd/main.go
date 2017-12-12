ae4482f9b6c2533b29dff5c6d02e660b2020e48e
Daniel Javorszky <daniel.javorszky@liferay.com>
2017-12-12 18:04:35 +0100

    Add example cmd

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"net/http"
 	"os"
 
 	"github.com/djavorszky/brink"
+	"golang.org/x/net/html"
 )
 
 func main() {
@@ -13,11 +16,34 @@ func main() {
 		os.Exit(1)
 	}
 
-	w, err := brink.NewWalker(os.Args[1])
+	cookies := make(map[string][]*http.Cookie)
+
+	cookies[os.Args[1]] = []*http.Cookie{
+		&http.Cookie{Domain: ".liferay.int", Name: "user", Value: "daniel.javorszky@liferay.com"},
+	}
+
+	c, err := brink.NewCrawlerWithOpts(os.Args[1], brink.CrawlOptions{Cookies: cookies})
 	if err != nil {
 		fmt.Printf("oops: %v\n", err)
 		os.Exit(1)
 	}
 
-	fmt.Println(w)
+	status, body, err := c.Crawl(os.Args[1])
+	if err != nil {
+		fmt.Printf("oops: %v", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("status code: %d", status)
+
+	z := html.NewTokenizer(bytes.NewBuffer(body))
+	for {
+		if z.Next() == html.ErrorToken {
+			// Returning io.EOF indicates success.
+			return
+		}
+
+		t := z.Token()
+		fmt.Printf("Type: %s, Data: %s\n", t.Type, t.Data)
+	}
 }
